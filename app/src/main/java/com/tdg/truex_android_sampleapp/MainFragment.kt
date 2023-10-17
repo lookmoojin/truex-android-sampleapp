b45c84dbb191c0926ce14a8b7836530c3720d626
Natcha Jintanasatien <Natcha.Jin@truedigital.com>
2023-10-17 14:25:23 +0700

    apply patch login

--- app/src/main/java/com/tdg/truex_android_sampleapp/MainFragment.kt
+++ app/src/main/java/com/tdg/truex_android_sampleapp/MainFragment.kt
@@ -1,16 +1,10 @@
-package com.tdg.truex_android_sampleapp
-
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.tdg.login.base.ViewModelFactory
-import com.tdg.login.presentation.LoginViewModel
 import com.tdg.truex_android_sampleapp.databinding.FragmentMainBinding
-import com.tdg.truex_android_sampleapp.injections.MainComponent
+import com.tdg.truex_android_sampleapp.injections.HomeComponent
 import javax.inject.Inject
 
 class MainFragment : Fragment() {
@@ -20,43 +14,31 @@ class MainFragment : Fragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
-
-    private val loginViewModel: LoginViewModel by viewModels { viewModelFactory }
+    private val mainViewModel: MainViewModel by viewModels { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        MainComponent.getInstance().inject(this)
         super.onCreate(savedInstanceState)
+        HomeComponent.getInstance().inject(this)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        viewmodelObserve()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        observeLoginViewModel()
-        initView()
+    private fun viewmodelObserve() {
+        mainViewModel.onLoginSuccess.observe(viewLifecycleOwner) {
+            binding.textViewResponse.text = it
+        }
     }
 
     private fun initView() = with(binding) {
         loginButton.setOnClickListener {
-            loginViewModel.login()
+            // login
+            mainViewModel.login()
         }
 
         nextButton.setOnClickListener {
-            findNavController().navigate(
-                MainFragmentDirections.actionMainFragmentToMenuFragment()
-            )
-        }
-    }
-
-    private fun observeLoginViewModel() = with(loginViewModel) {
-        onShowErrorToast().observe(viewLifecycleOwner) {
-        }
-    }
-}
