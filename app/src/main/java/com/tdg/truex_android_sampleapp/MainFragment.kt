9795fc486061c0f3339695d05e48f8e3206b71c6
Natcha Jintanasatien <Natcha.Jin@truedigital.com>
2023-10-18 12:16:24 +0700

    copy jwt

--- app/src/main/java/com/tdg/truex_android_sampleapp/MainFragment.kt
+++ app/src/main/java/com/tdg/truex_android_sampleapp/MainFragment.kt
@@ -1,11 +1,14 @@
 package com.tdg.truex_android_sampleapp
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -45,10 +48,10 @@ class MainFragment : Fragment() {
 
     private fun viewModelObserve() = with(mainViewModel) {
         onLoginSuccess.observe(viewLifecycleOwner) {
-            binding.textViewResponse.text = it
+            binding.responseTextView.text = it
         }
         onLoginFailed.observe(viewLifecycleOwner) {
-            binding.textViewResponse.text = it
+            binding.responseTextView.text = it
         }
     }
 
@@ -74,6 +77,16 @@ class MainFragment : Fragment() {
             findNavController().navigate(
                 MainFragmentDirections.actionMainFragmentToMenuFragment()
             )
+        }
+
+        copyButton.setOnClickListener {
+            Toast.makeText(context, "Copied to clipboard.", Toast.LENGTH_SHORT).show()
+            context?.let { context ->
+                val clipboard =
+                    context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText("Copied Text", binding.responseTextView.text)
+                clipboard.setPrimaryClip(clip)
+            }
         }
     }
 
